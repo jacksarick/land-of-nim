694c431c6d775e36d271261e1057345c4a9485dd
Jack Sarick <jacksarick@gmail.com>
2017-04-28 17:26:55 -0400

    I have simultaneously gotten far and nowhere

--- stack.nim
+++ stack.nim
@@ -1,11 +1,11 @@
 import asyncnet, asyncdispatch
 
 type
-  Stack = seq[string]
+  Stack = tuple[p1: string, p2: string]
 
 var
   clients {.threadvar.}: seq[AsyncSocket]
-  instructions: Stack = @[]
+  cmds: Stack = (p1: "", p2: "")
 
 proc processClient(client: AsyncSocket) {.async.} =
   await client.send("connected\c\L")
@@ -13,7 +13,10 @@ proc processClient(client: AsyncSocket) {.async.} =
   while true:
     let line = await client.recvLine()
     if line.len == 0: break
-       
+    for c in clients:
+      if c != client:
+        await c.send($find(clients, client) & ": " & line & "\c\L")
+    
 
 proc serve() {.async.} =
   clients = @[]
@@ -23,10 +26,12 @@ proc serve() {.async.} =
   server.listen()
   
   while true:
-    let client = await server.accept()
-    clients.add client
+    if clients.len < 3:
+      echo clients.len
+      let client = await server.accept()
+      clients.add client
     
-    asyncCheck processClient(client)
+      asyncCheck processClient(client)
 
 asyncCheck serve()
 runForever()
